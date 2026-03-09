995f623804f4afbffe22c357e878d20c5e7d5b00
Sana K <kallaparisana@gmail.com>
2026-03-09 21:21:30 +0530

    initial commit

---
+++ backend/model/OrdersModel.js
@@ -0,0 +1,7 @@
+const { model } = require("mongoose");
+
+const { OrdersSchema } = require("../schemas/OrdersSchema");
+
+const OrdersMOdel = new model("order", OrdersSchema);
+
+module.exports = { OrdersMOdel };
